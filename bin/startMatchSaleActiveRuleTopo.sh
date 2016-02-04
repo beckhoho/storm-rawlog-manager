f4899b7810c1103a1690078f56c233d2525bed5b
gongice <gshiwen@gmail.com>
2016-02-04 21:06:54 +0800

    add conf&bin

---
+++ bin/startMatchSaleActiveRuleTopo.sh
@@ -0,0 +1,1 @@
+storm jar XJ_atom_events-0.0.1-SNAPSHOT.jar com.nl.event.topo.MatchSaleActiveRuleTopo /home/stormdev/storm/conf/match-sale-active-rule-topo.yaml
---
+++ bin/startMcCaptureAtomEventTopo.sh
@@ -0,0 +1,1 @@
+storm jar XJ_atom_events-0.0.1-SNAPSHOT.jar com.nl.event.topo.McCaptureAtomEventTopo /home/stormdev/storm/conf/mc-capture-atom-event-topo.yaml
